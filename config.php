8120b969f02bee86eebd845d800a6ff59a875567
nheek <nickjameshipol@gmail.com>
2024-11-24 16:33:24 +0100

    Update environment variable handling and add .dockerignore

--- config.php
+++ config.php
@@ -1,10 +1,8 @@
 <?php
-$ENV = parse_ini_file('.env');
-
-$sqlHost = $ENV['MYSQL_HOST'];
-$sqlUser = $ENV['MYSQL_USER'];
-$sqlPass = $ENV['MYSQL_PASSWORD'];
-$sqlName = $ENV['MYSQL_DATABASE'];
+$sqlHost = getenv('MYSQL_HOST');
+$sqlUser = getenv('MYSQL_USER');
+$sqlPass = getenv('MYSQL_PASSWORD');
+$sqlName = getenv('MYSQL_DATABASE');
 
 $sqlConnect = new mysqli($sqlHost, $sqlUser, $sqlPass, $sqlName);
 $sqlConnect->set_charset("utf8");
